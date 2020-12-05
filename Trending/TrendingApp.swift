7bd65a43f31dedd14e4393ea1ac1aa5a2f0a69cb
Alex Skorulis <skorulis@gmail.com>
2020-12-05 17:18:42 +1100

    Initial Commit

---
+++ Trending/TrendingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendingApp.swift
+//  Trending
+//
+//  Created by Alexander Skorulis on 5/12/20.
+//
+
+import SwiftUI
+
+@main
+struct TrendingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
